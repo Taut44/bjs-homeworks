9934f3db22dea9ed1aee0277a4a56b41da93c4db
Taut <nrdunaev@gmail.com>
2020-08-18 14:35:39 +0300

    Edit some code

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,15 +1,15 @@
 function getResult(a,b,c){
-    let discr = b**2 - 4*a*c;
-    let massiv = [];
-    if (discr > 0) {
-        let x1 = (-b + Math.sqrt(discr))/2*a;
-        let x2 = (-b - Math.sqrt(discr))/2*a;
-        massiv.push(x1, x2);
-    } else if (discr === 0) {
-        let x1 = (-b + Math.sqrt(discr))/2*a;
-        massiv.push(x1);
+    let discriminant = b**2 - 4*a*c;
+    let array = [];
+    if (discriminant > 0) {
+        let x1 = (-b + Math.sqrt(discriminant))/2*a;
+        let x2 = (-b - Math.sqrt(discriminant))/2*a;
+        array.push(x1, x2);
+    } else if (discriminant === 0) {
+        let x1 = (-b + Math.sqrt(discriminant))/2*a;
+        array.push(x1);
     };
-    return massiv;
+    return array;
 };
 
 function getAverageMark(marks){
@@ -20,7 +20,8 @@ function getAverageMark(marks){
     }
     for (let i = 0; i < marks.length; i++) {
       sum += marks[i];
-      if (marks.length > 4) {
+      const excessMarks = 5;
+      if (marks.length > excessMarks) {
         console.log('Оценок больше пяти')
         marks.splice (5);
         averageMarks = sum / marks.length;
@@ -33,12 +34,10 @@ function getAverageMark(marks){
  
 
 function askDrink(name,dateOfBirthday){
-    let result;
     const age = new Date().getFullYear() - dateOfBirthday.getFullYear()
     if (age >= 18) {
-        result = `Не желаете ли олд-фэшн, ${name}?`;
+        return `Не желаете ли олд-фэшн, ${name}?`;
     } else {
-        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
     }
-    return result;
 }
