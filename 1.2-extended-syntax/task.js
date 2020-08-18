814572cdaa4718546108c1ea14d95343517cf25a
Taut <nrdunaev@gmail.com>
2020-08-18 15:12:45 +0300

    Edit some code

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,39 +1,35 @@
-function getResult(a,b,c){
-    let discriminant = b**2 - 4*a*c;
+function getResult(a, b, c) {
+    let discriminant = b ** 2 - 4 * a * c;
     let array = [];
     if (discriminant > 0) {
-        let x1 = (-b + Math.sqrt(discriminant))/2*a;
-        let x2 = (-b - Math.sqrt(discriminant))/2*a;
+        let x1 = (-b + Math.sqrt(discriminant)) / 2 * a;
+        let x2 = (-b - Math.sqrt(discriminant)) / 2 * a;
         array.push(x1, x2);
     } else if (discriminant === 0) {
-        let x1 = (-b + Math.sqrt(discriminant))/2*a;
+        let x1 = (-b + Math.sqrt(discriminant)) / 2 * a;
         array.push(x1);
     };
     return array;
 };
 
-function getAverageMark(marks){
+function getAverageMark(marks) {
     let sum = 0;
-    let averageMarks;
+    const excessMarks = 5;
     if (marks.length === 0) {
-        averageMarks = 0;
+        return 0;
     }
     for (let i = 0; i < marks.length; i++) {
-      sum += marks[i];
-      const excessMarks = 5;
-      if (marks.length > excessMarks) {
-        console.log('Оценок больше пяти')
-        marks.splice (5);
-        averageMarks = sum / marks.length;
-      } else {
-        averageMarks = sum / marks.length;
-      };
+        sum += marks[i];
+        if (marks.length > excessMarks) {
+            console.log('Оценок больше пяти')
+            marks.splice(5);
+        };
     };
-    return averageMarks;
+    return sum / marks.length;
 };
- 
 
-function askDrink(name,dateOfBirthday){
+
+function askDrink(name, dateOfBirthday) {
     const age = new Date().getFullYear() - dateOfBirthday.getFullYear()
     if (age >= 18) {
         return `Не желаете ли олд-фэшн, ${name}?`;
