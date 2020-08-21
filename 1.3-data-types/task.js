e5133f54b84c90a1acd2dc117df9ff8b2d5643fc
Taut <nrdunaev@gmail.com>
2020-08-21 13:22:56 +0300

    Do 1.3 homework

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,24 @@
+'use strict';
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    const remainder = amount - contribution;
+    function countMonths(startDate, endDate) {
+        let months;
+        months = (endDate.getFullYear() - startDate.getFullYear()) * 12;
+        months -= startDate.getMonth();
+        months += endDate.getMonth();
+        return months <= 0 ? 0 : months;
+    }
+    const nowDate = new Date();
+    const paymentMonths = countMonths(nowDate, date)
+    const monthlyPercent = percent / 100 / 12;
+    const payment = remainder * (monthlyPercent + monthlyPercent / (Math.pow(1 + monthlyPercent, paymentMonths) - 1));
+    console.log(+(payment * paymentMonths).toFixed(2))
+    return +(payment * paymentMonths).toFixed(2);
 }
 
+
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    console.log(`Привет, мир! Меня зовут ${name ? name : 'Аноним'}`)
+    return `Привет, мир! Меня зовут ${name ? name : 'Аноним'}`
 }
