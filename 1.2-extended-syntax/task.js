5fb3e028ea8f4906e68c7c5a94fc2f8d2b0d8be1
Taut <nrdunaev@gmail.com>
2020-08-24 15:17:07 +0300

    Edit some code

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -30,25 +30,21 @@ function getAverageMark(marks) {
 
 // Для ввода только значений а не массива
 
-/* function getAverageMark(marks) {
-    let array = [];
-    let sum = 0;
-    const excessMarks = 5;
-    for(let i=0; i < arguments.length; i++){
-        array.push(arguments[i]);
+/* function getAverageMark(...marks) {
+  let sum = 0;
+  const excessMarks = 5;
+  if (marks.length === 0) {
+    return 0;
+  };
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+    if (marks.length > excessMarks) {
+      console.log('Оценок больше пяти')
+      marks.splice(5);
     };
-    if (array.length === 0) {
-      return 0;
-    };
-    for (let i = 0; i < array.length; i++) {
-      sum += array[i];
-      if (array.length > excessMarks) {
-        console.log('Оценок больше пяти')
-        array.splice(5);
-      };
-    };
-      return sum / array.length;
-  }; */
+  };
+    return sum / marks.length;
+}; */
 
 
 function askDrink(name, dateOfBirthday) {
