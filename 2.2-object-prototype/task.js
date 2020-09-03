f6f961dbe302aa046b744ebaa58affacf41ea6c3
Taut <nrdunaev@gmail.com>
2020-09-03 14:37:48 +0300

    Edit some code

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,11 @@
 String.prototype.isPalindrome = function() {
     let str = this.toLowerCase();
     for (var i = 0; i < str.length / 2; i++) {
-     if (str[i] === str[str.length - 1 - i]) {
-         return true;
+     if (str[i] !== str[str.length - 1 - i]) {
+         return false;
      };
    };
-   return false;
+   return true;
   };
   
   
