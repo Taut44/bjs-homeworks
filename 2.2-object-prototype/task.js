75f4d9ce9aff71275145c0bfdf1da93be052ae9f
Taut <nrdunaev@gmail.com>
2020-09-03 14:49:56 +0300

    Edit some code

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,28 +1,27 @@
-String.prototype.isPalindrome = function() {
-    let str = this.toLowerCase();
-    for (var i = 0; i < str.length / 2; i++) {
-     if (str[i] !== str[str.length - 1 - i]) {
-         return false;
-     };
-   };
-   return true;
-  };
-  
-  
-  function getAverageMark(marks) {
-    let average = 0;
-    for (let i = 0; i < marks.length; i++) {
-      average += marks[i] / marks.length;
+String.prototype.isPalindrome = function () {
+  let str = this.toLowerCase();
+  for (var i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
     };
-    let roundedAverage = Math.round(average);
-    return roundedAverage;
+    return true;
   };
-  
-  function checkBirthday(birthday) {
-      let now = Date.now();
-      let userBirthday = Date.now(birthday);
-      let diff = userBirthday - now
-      let age = diff / 31557600000;
-      let verdict = (age > 18 ? true : false);
-      return age > 18 ? `Не желаете ли олд-фэшн?` : `Сожалею, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+};
+
+
+function getAverageMark(marks) {
+  let average = 0;
+  for (let i = 0; i < marks.length; i++) {
+    average += marks[i] / marks.length;
   };
+  let roundedAverage = Math.round(average);
+  return roundedAverage;
+};
+
+function checkBirthday(birthday) {
+  let now = Date.now();
+  let userBirthday = +new Date(birthday);
+  let diff = now - userBirthday;
+  let age = diff / 31557600000;
+  return age > 18 ? true : false;
+};
